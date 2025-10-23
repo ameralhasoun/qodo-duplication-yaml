28b571b46b59a03f312596b97d1349c7ea7a832e
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-10-23 12:24:24 +0200

    add calculator class

---
+++ Calculator.cs
@@ -0,0 +1,7 @@
+public class Calculator
+{
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            int result1 = calc.AddNumbers(5, 3);
+
+            Console.WriteLine($"AddNumbers result: {result1}");
+        }
+    }
+}
