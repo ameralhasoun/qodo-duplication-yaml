32719aa0abfe4b166d9aec303a96b197de38527b
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-10-23 12:29:04 +0200

    add rekenmachine

--- Program.cs
+++ Program.cs
@@ -7,10 +7,12 @@ namespace Example
         static void Main(string[] args)
         {
             Calculator calc = new Calculator();
-
             int result1 = calc.AddNumbers(5, 3);
-
             Console.WriteLine($"AddNumbers result: {result1}");
+
+            Rekenmachine rekenmachine = new Rekenmachine();
+            int result2 = rekenmachine.Optellen(5, 3);
+            Console.WriteLine($"AddNumbers result: {result2}");
         }
     }
 }
